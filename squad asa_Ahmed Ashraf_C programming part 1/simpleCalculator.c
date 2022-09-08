03f6339c1b90282bbccc8c8c8d3467e8172dae3e
AhmedAshraf4 <ahmedashraf2452001@gmail.com>
2022-09-08 19:56:46 +0200

    part 1

---
+++ squad asa_Ahmed Ashraf_C programming part 1/simpleCalculator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int firstNumber;
+    int secondNumber;
+    do{
+        scanf(%f, &firstNumber);
+        scanf(%f, &secondNumber);
+        if(!isdigit(firstNumber) || !isdigit(secondNumber)){
+            printf("Please enter digit values")
+        }
+    }
+}
