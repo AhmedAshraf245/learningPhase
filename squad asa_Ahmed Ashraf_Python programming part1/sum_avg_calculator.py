954e199817a21b1c08aa732d48e06c4d21f6120d
AhmedAshraf4 <ahmedashraf2452001@gmail.com>
2022-09-15 00:35:56 +0200

    part1

---
+++ squad asa_Ahmed Ashraf_Python programming part1/sum_avg_calculator.py
@@ -0,0 +1,9 @@
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+num3 = input("Enter third number: ")
+
+sum = float(num1) + float(num2) + float(num3)
+avg = sum / 3
+
+print("Sum = " + str(sum))
+print("Average = " + str(avg))
