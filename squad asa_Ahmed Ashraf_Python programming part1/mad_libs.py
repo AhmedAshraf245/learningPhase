54e57b8f7628b3767884d75fb2b208f6171af61f
AhmedAshraf4 <ahmedashraf2452001@gmail.com>
2022-09-15 19:38:24 +0200

    changes

--- squad asa_Ahmed Ashraf_Python programming part1/mad_libs.py
+++ squad asa_Ahmed Ashraf_Python programming part1/mad_libs.py
@@ -3,6 +3,5 @@ song = input("Enter a song: ")
 celebrity_first_name = input("Enter celebrity's first name : ")
 celebrity_last_name = input("Enter celebrity's last name : ")
 
-print("Roses are " + colour)
-print(song + " are blue")
-print("I love " + celebrity_first_name + " " + celebrity_last_name)
+print("Roses \t are " + colour)
+print(song + " are blue\n" + "I love " + celebrity_first_name + " " + celebrity_last_name)
