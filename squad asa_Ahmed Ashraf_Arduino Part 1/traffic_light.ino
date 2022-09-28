b21403d501ca261a53192f12f659bff3a2149277
AhmedAshraf4 <ahmedashraf2452001@gmail.com>
2022-09-28 18:56:31 +0200

    added delay

--- squad asa_Ahmed Ashraf_Arduino Part 1/traffic_light.ino
+++ squad asa_Ahmed Ashraf_Arduino Part 1/traffic_light.ino
@@ -1,7 +1,3 @@
-/*
-Simulation file on tinkercad: https://www.tinkercad.com/things/27YhXAipPv1
-*/
-
 #define button 2
 #define green_led 12
 #define yellow_led 11
@@ -23,40 +19,36 @@ void setup() {
 
 void loop() {
   button_current = digitalRead(button); 
-  if(button_current == HIGH && button_previous == LOW)
-  {
-    if(green_state==LOW && yellow_state==LOW && red_state==LOW)
-    {
-      green_state=HIGH;
+  if(button_current == HIGH && button_previous == LOW){
+
+  if(green_state==LOW && yellow_state==LOW && red_state==LOW){
+    green_state=HIGH;
+  }
+  else if(green_state==HIGH){
+    green_state=LOW;
+    yellow_state=HIGH;
+    red_or_green = true;
+  }
+  else if(red_state==HIGH){
+    red_state=LOW;
+    yellow_state=HIGH;
+    red_or_green = false;
+  }
+  else if(yellow_state==HIGH){
+    if(red_or_green == true){
+    yellow_state=LOW;
+    red_state=HIGH;
     }
-    else if(green_state==HIGH)
-    {
-      green_state=LOW;
-      yellow_state=HIGH;
-      red_or_green = true;
+    else{
+    yellow_state=LOW;
+    green_state=HIGH;
     }
-    else if(red_state==HIGH)
-    {
-      red_state=LOW;
-      yellow_state=HIGH;
-      red_or_green = false;
-    }
-    else if(yellow_state==HIGH)
-    {
-      if(red_or_green == true)
-      {
-        yellow_state=LOW;
-        red_state=HIGH;
-      }
-      else
-      {
-        yellow_state=LOW;
-        green_state=HIGH;
-      }
-    }
+  }
   }
   digitalWrite(green_led,green_state);
   digitalWrite(yellow_led,yellow_state);
   digitalWrite(red_led,red_state);
   button_previous = button_current;
+  delay(10);
 }
+
